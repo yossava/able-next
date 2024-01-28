b0082245b30ef21edeaa2f32deaaced1b981c452
Yossava Adhi <yossava.adhi@insurediy.com>
2024-01-28 13:32:05 +0700

    update tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,35 +5,35 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
         brand: {
           accent: '#5AD769',
-          main: '#324BC3',
+          main: '#324BC3'
         },
         text: {
           primary: '#FFFFFF',
-          secondary: '#0C0F19',
+          secondary: '#0C0F19'
         },
         background: {
-          primary: '#0C0F19',
+          primary: '#0C0F19'
         },
         primary: '#FDB833',
         primaryFocus: '#FDC43F',
         secondary: '#fd7014',
         tertiary: '#F4D41C',
-        grey: '#f2f4f6',
+        grey: '#f2f4f6'
       },
       fontFamily: {
         sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
         mono: [...defaultTheme.fontFamily.mono],
-        poppins: ['var(--font-poppins)'],
-      },
-    },
+        poppins: ['var(--font-poppins)']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
